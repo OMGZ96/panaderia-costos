0dd9ea2b94e8b1ed8161c3a1819808edfd2c1e33
OMGZ96 <tu-email@example.com>
2025-12-06 17:06:06 -0500

    fix: bundle all dependencies locally (remove picsum)

---
+++ electron/main.js
@@ -0,0 +1,40 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+
+function createWindow() {
+    const win = new BrowserWindow({
+        width: 1200,
+        height: 800,
+        webPreferences: {
+            nodeIntegration: true,
+            contextIsolation: false,
+        },
+        icon: path.join(__dirname, '../public/icon.svg') // Using the SVG we created
+    });
+
+    const isDev = process.env.NODE_ENV === 'development';
+
+    if (isDev) {
+        win.loadURL('http://localhost:3000');
+        win.webContents.openDevTools();
+    } else {
+        // In production, load the local index.html from dist
+        win.loadFile(path.join(__dirname, '../dist/index.html'));
+    }
+}
+
+app.whenReady().then(() => {
+    createWindow();
+
+    app.on('activate', () => {
+        if (BrowserWindow.getAllWindows().length === 0) {
+            createWindow();
+        }
+    });
+});
+
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') {
+        app.quit();
+    }
+});
